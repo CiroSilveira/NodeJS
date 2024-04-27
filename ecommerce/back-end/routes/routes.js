8d192f91a219b805ff3127d64ba896cedb773b36
CiroSilveira <ciroteixeirasilveira@gmail.com>
2024-04-27 16:02:27 -0300

    crud finalizado e compreendido

--- ecommerce/back-end/routes/routes.js
+++ ecommerce/back-end/routes/routes.js
@@ -3,57 +3,63 @@ const crypto = require('crypto');
 const router = express.Router();
 const pool = require('../dbConfig');
 
-const products = pool.query('select * from products');
+const products = []
 
 router.get('/', async (req, res) => {
-    const products = await pool.query('select * from products');
-    res.send(products.rows);
+    const {rows} = await pool.query('select * from products order by id');
+    res.send(rows);
 })
 
 router.get('/:id', async (req, res) => {
-    // Faço a query no db e retorno o valor dela para a constante products
-    const products = await pool.query('select * from products');
-    // Pocuro por um produto no array products cujo id coincide com o id que veio no parâmetro da URL 'req.params.id'
+    // Faço a query no DB e retorno o valor dela para a constante rows que é um array de objetos
+    const {rows} = await pool.query('select * from products where id = $1', [req.params.id]);
+    // Pocuro por um produto no array rows cujo id coincide com o id que veio no parâmetro da URL 'req.params.id'
     // A constante products é um objeto que tem um array chamado rows. Nesse array vem os itens cadastrados na minha tabela
-    const product = products.rows.find(product => product.id == req.params.id);
+    // const product = products.rows.find(product => product.id == req.params.id);
     // Se não houver produto cadastrado com o id passado via parâmetro URL eu devolvo um 404 e respondo a mensagem do res.send()
-    if(!product) res.status(404).send('Produto não encontrado!');
-    // Se houver produto cadastrado com o id passado via parâmetro URL eu devolvo os dados do produto
-    res.send(product);
+    if(rows.length === 0) {
+        res.status(404).send('Produto não encontrado!');
+    } else {// Se houver produto cadastrado com o id passado via parâmetro URL eu devolvo os dados do produto
+        res.send(rows);
+    }
 })
 
 // CRUD (CREATE - POST) (READ - GET) (UPDATE - PUT) (DELETE - DELETE)
 
 // [POST] - CADASTRA UM NOVO PRODUTO
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
     const product = req.body;
-    if(!product.name || !product.category || !product.price) {
+    
+    if(!product.id || !product.name || !product.category || !product.price) {
         res.status(400).send('Está faltando dados do produto.');
     } else {
-        products.push({
-            id: crypto.randomUUID(),
-            ...product 
-        });
-        res.status(201).send('O produto foi cadastrado com sucesso!');
+        const {rows} = await pool.query('insert into products (id, name, category, price) values ($1, $2, $3, $4) RETURNING *', [product.id, product.name, product.category, product.price]);
+
+        res.status(201).json({
+            status: 'Produto cadastrado com sucesso!',
+            data: rows
+        })
     }
 })
 
-router.delete('/delete/:id', (req, res) => {
-    const index = products.findIndex(product => product.id == req.params.id);
-    console.log(index);
-    products.splice(index, 1);
-    res.send('Produto excluído com sucesso!');
-})
-
-router.put('/update/:id', (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
-    const upd = req.body;
-    const index = products.findIndex(product => product.id == id);
-    products[index] = {
-        ...products[index],
-        ...upd
-    }
-    res.status(200).send('Produto atualizado com sucesso!')
+    const {rows} = await pool.query('delete from products where id = $1', [id]);
+
+    res.status(200).json({
+        status: 'Produto excluído com sucesso!',
+        data: rows
+    })
+})
+
+router.put('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const {rows} = await pool.query('update products set name = $2, category = $3, price = $4 where id = $1 returning *', [id, req.body.name, req.body.category, req.body.price])
+
+    res.status(200).json({
+        status: 'Produto atualizado com sucesso!',
+        data: rows
+    })
 })
 
 
